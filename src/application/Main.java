741920b8eb1f9ef65bffef98463e5e0dadf9e863
Unknown <florian.brossard@edu.univ-fcomte.fr>
2017-08-31 00:40:00 +0200

    deleted hard coded window size, ResourcesHelper no longer needed

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,22 +1,27 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
-import application.helpers.*;
 
+//TODO Documentation
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(ResourcesHelper.getResource("gui/GUI.fxml"));
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(ResourcesHelper.getResource("gui/application.css").toExternalForm());
+			ClassLoader classLoader = getClass().getClassLoader();
+			BorderPane root = (BorderPane)FXMLLoader.load(classLoader.getResource("application/gui/GUI.fxml"));
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(classLoader.getResource("application/gui/application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setResizable(false);
 			primaryStage.show();
+			//this makes all stages close and the app exit when the main stage is closed
+			primaryStage.setOnCloseRequest(e -> Platform.exit());
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
