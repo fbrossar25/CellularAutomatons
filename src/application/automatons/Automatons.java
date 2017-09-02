febce1bd5b4b4c492bf91bee5a2ca206c7badf09
Unknown <florian.brossard@edu.univ-fcomte.fr>
2017-09-02 21:42:42 +0200

    Automatons.java code reformating
    Forgot this one

--- src/application/automatons/Automatons.java
+++ src/application/automatons/Automatons.java
@@ -1,27 +1,27 @@
 package application.automatons;
 
-public enum Automatons{
-	GAME_OF_LIFE("Conway's Game of Life"),
-	LANGTONS_ANT("Langton's Ant");
-	
-	private final String display;
-	private Automatons(String s) {
-		display = s;
-	}
-	
-	public CellularAutomaton getInstance() {
-		switch(this) {
-			case GAME_OF_LIFE:
-				return new GameOfLife();
-			case LANGTONS_ANT:
-				return new LangtonsAnt();
-			default:
-				return null;
-		}
-	}
-	
-	@Override
-	public String toString() {
-		return display;
-	}
+public enum Automatons {
+    GAME_OF_LIFE("Conway's Game of Life"), LANGTONS_ANT("Langton's Ant");
+
+    private final String display;
+
+    private Automatons(String s) {
+        display = s;
+    }
+
+    public CellularAutomaton getInstance() {
+        switch (this) {
+        case GAME_OF_LIFE:
+            return new GameOfLife();
+        case LANGTONS_ANT:
+            return new LangtonsAnt();
+        default:
+            return null;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return display;
+    }
 }
