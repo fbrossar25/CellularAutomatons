53ef28e11c7079054cdf451e04cd9e6b1cfcd4ba
Unknown <florian.brossard@edu.univ-fcomte.fr>
2017-09-02 21:47:57 +0200

    GUIController -> automaton's canvas & handlers
    GUIController now using CellularAutomatonCanvas & has workig handlers for AutomatonEvents

--- src/application/gui/GUIController.java
+++ src/application/gui/GUIController.java
@@ -1,93 +1,164 @@
 package application.gui;
 
 import java.util.ResourceBundle;
+
 import application.automatons.Automatons;
 import application.automatons.CellularAutomaton;
+import application.events.AutomatonEvent;
 import application.helpers.Drawings;
 import javafx.fxml.FXML;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Slider;
+import javafx.scene.layout.BorderPane;
 
 public class GUIController {
-	private CellularAutomaton automaton;
-	@FXML
-	Slider speedSlider;
-	@FXML
-	ResourceBundle resourcesBundle;
-	@FXML
-	Canvas canvas;
-	@FXML
-	ComboBox<Automatons> automatonChooser;
-	
-	
-	
-	public GUIController() {
-		
-	}
-	
-	@FXML
-	public void initialize() {
-		automatonChooser.getItems().setAll(Automatons.values());
-		
-		speedSlider.valueProperty().addListener((observable,oldValue,newValue)->{
-			speedChanged(newValue.intValue());
-		});
-		Drawings.drawBackground(canvas);
-	}
-	
-	@FXML
-	public void automatonChanged() {
-		//System.out.println("Automaton changed -> "+automatonChooser.getValue());
-		setAutomaton(automatonChooser.getValue().getInstance());
-		if(automaton == null)
-			return;
-		
-		System.out.println(automaton);
-		Drawings.drawCellularAutomaton(canvas, automaton);
-	}
-	
-	@FXML
-	public void nextButtonPressed() {
-		if(automaton == null)
-			return;
-		automaton.next();
-		System.out.println("============= GENERATION "+automaton.getGeneration()+" =============");
-		System.out.println(automaton);
-		Drawings.drawCellularAutomaton(canvas, automaton);
-	}
-	
-	@FXML
-	public void startButtonPressed() {
-		if(automaton == null)
-			return;
-		if(automaton.isPaused())
-			automaton.start();
-		else
-			automaton.pause();
-	}
-	
-	@FXML
-	public void resetButtonPressed() {
-		if(automaton == null)
-			return;
-		automaton.clear();
-	}
-	
-	@FXML
-	public void canvasClicked() {
-		
-	}
-	
-	public void speedChanged(int value) {
-		//System.out.println("Speed -> "+value);
-		if(automaton == null)
-			return;
-		automaton.setSpeed(value);
-	}
-	
-	public void setAutomaton(CellularAutomaton automaton) {
-		this.automaton = automaton;
-	}
+    public static final double      DEFAULT_CANVAS_SIZE = 400.0;
+    private CellularAutomaton       automaton;
+    private CellularAutomatonCanvas canvas;
+
+    @FXML
+    Slider                          speedSlider;
+    @FXML
+    ResourceBundle                  resourcesBundle;
+    @FXML
+    BorderPane                      mainBorderPane;
+    @FXML
+    ComboBox<Automatons>            automatonChooser;
+    @FXML
+    Button                          nextButton;
+    @FXML
+    Button                          startButton;
+
+    public GUIController() {
+        canvas = new CellularAutomatonCanvas(DEFAULT_CANVAS_SIZE, DEFAULT_CANVAS_SIZE, null);
+    }
+
+    @FXML
+    public void initialize() {
+        automatonChooser.getItems().setAll(Automatons.values());
+
+        speedSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
+            speedChanged(newValue.intValue());
+        });
+
+        mainBorderPane.setCenter(canvas);
+        mainBorderPane.addEventHandler(AutomatonEvent.AUTOMATON_STARTED, (event) -> handleAutomatonStarted());
+        mainBorderPane.addEventHandler(AutomatonEvent.AUTOMATON_ENDED, (event) -> handleAutomatonEnded());
+        mainBorderPane.addEventHandler(AutomatonEvent.AUTOMATON_PAUSED, (event) -> handleAutomatonPaused());
+        mainBorderPane.addEventHandler(AutomatonEvent.AUTOMATON_RESET, (event) -> handleAutomatonReset());
+        mainBorderPane.addEventHandler(AutomatonEvent.AUTOMATON_STEP, (event) -> handleAutomatonStep());
+
+        adaptCanvasSize();
+        // Drawings.drawBackground(canvas);
+    }
+
+    @FXML
+    public void adaptCanvasSize() {
+        // TODO adapt canvas size with computed one
+        canvas.setWidth(DEFAULT_CANVAS_SIZE);
+        canvas.setHeight(DEFAULT_CANVAS_SIZE);
+    }
+
+    private void handleAutomatonStarted() {
+        // System.out.println("Automaton started !");
+    }
+
+    private void handleAutomatonEnded() {
+        // System.out.println("Automaton ended");
+    }
+
+    private void handleAutomatonPaused() {
+        // System.out.println("Automaton paused");
+    }
+
+    private void handleAutomatonReset() {
+        // System.out.println("Automaton reset");
+    }
+
+    private void handleAutomatonStep() {
+        // System.out.println("Automaton step");
+    }
+
+    @FXML
+    public void automatonChanged() {
+        if (automaton != null) {
+            automaton.end();
+            automaton.clear();
+        }
+        goPauseState();
+        setAutomaton(automatonChooser.getValue().getInstance());
+    }
+
+    @FXML
+    public void nextButtonPressed() {
+        if (automaton == null)
+            return;
+        automaton.next();
+        // System.out.println(automaton);
+        Drawings.drawCellularAutomaton(canvas, automaton);
+    }
+
+    @FXML
+    public void startButtonPressed() {
+        if (automaton == null)
+            return;
+        if (automaton.isPaused()) {
+            goStartState();
+        } else {
+            goPauseState();
+        }
+    }
+
+    public void goPauseState() {
+        if (automaton != null)
+            automaton.pause();
+        nextButton.setDisable(false);
+        startButton.setText("Start");
+    }
+
+    public void goStartState() {
+        if (automaton != null)
+            automaton.start();
+        nextButton.setDisable(true);
+        startButton.setText("Pause");
+    }
+
+    @FXML
+    public void resetButtonPressed() {
+        if (automaton == null)
+            return;
+        goPauseState();
+        automaton.reset();
+    }
+
+    @FXML
+    public void canvasClicked() {
+        // TODO switch between alive and dead cells
+    }
+
+    @FXML
+    public void handleResizeCommand() {
+        // TODO resize automaton
+    }
+
+    public void speedChanged(int value) {
+        // System.out.println("Speed -> "+value);
+        if (automaton == null)
+            return;
+        automaton.setSpeed(value);
+    }
+
+    public void setAutomaton(CellularAutomaton newAutomaton) {
+        if (this.automaton != null)
+            this.automaton.removeAllEventTargets();
+
+        this.automaton = newAutomaton;
+
+        if (this.automaton != null) {
+            this.automaton.addEventTarget(mainBorderPane);
+            automaton.setSpeed(speedSlider.valueProperty().intValue());
+        }
+        canvas.setAutomaton(this.automaton);
+    }
 }
