6280fa4962b442ed1d9bee7fea6e1c9ff3455a41
Unknown <florian.brossard@edu.univ-fcomte.fr>
2017-09-02 21:46:53 +0200

    CellularAutomaton refactoring
    Now fire events and has an updater with system to roughly reschedule it

--- src/application/automatons/CellularAutomaton.java
+++ src/application/automatons/CellularAutomaton.java
@@ -1,134 +1,203 @@
 package application.automatons;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import application.events.AutomatonEvent;
 import application.helpers.BoolGrid;
+import javafx.event.Event;
+import javafx.event.EventTarget;
+import javafx.event.EventType;
 
 public abstract class CellularAutomaton {
-	protected BoolGrid currentGeneration;
-	protected BoolGrid nextGeneration;
-	protected int speed = 1;
-	protected boolean paused = false;
-	protected int generation = 0;
-	protected boolean end = false;
-	
-	public CellularAutomaton() {
-		this(BoolGrid.DEFAULT_SIZE, BoolGrid.DEFAULT_SIZE);
-	}
-	
-	public CellularAutomaton(int size) {
-		this(size,size);
-	}
-	
-	public CellularAutomaton(int rows, int cols) {
-		if(rows < 1 || cols < 1)
-			throw new IllegalArgumentException("Invalid dimensions : ("+rows+","+cols+")");
-		this.currentGeneration = new BoolGrid(rows,cols);
-		this.nextGeneration = new BoolGrid(rows,cols);
-		init();
-	}
-	
-	protected abstract void init();
-	
-	public void clear() {
-		this.currentGeneration.reset();
-		this.nextGeneration.reset();
-		this.generation = 0;
-	}
-	
-	public boolean isEnd() {
-		return this.end;
-	}
-	
-	public void next() {
-		if(end)
-			return;
-		nextStep();
-		this.generation++;
-		this.currentGeneration.initWithOther(this.nextGeneration);
-		//this.nextGeneration.reset();
-	}
-	
-	protected abstract void nextStep();
-	
-	public void pause() {
-		this.paused = true;
-	}
-	
-	public void start() {
-		this.paused = false;
-	}
-	
-	public boolean isPaused() {
-		return this.paused;
-	}
-	
-	public int getSpeed() {
-		return this.speed;
-	}
-	
-	public void setSpeed(int value) {
-		if(value < 0) {
-			this.speed = 1;
-			this.pause();
-		}
-		else {
-			this.speed = value;
-		}
-	}
-	
-	public int getGeneration() {
-		return this.generation;
-	}
-	
-	public int rows() {
-		return this.currentGeneration.rows();
-	}
-	
-	public int cols() {
-		return this.currentGeneration.cols();
-	}
-	
-	public boolean isCellPopulated(int row, int col) {
-		boolean b;
-		try {
-			b = this.currentGeneration.get(row, col);
-		}catch(IndexOutOfBoundsException e){
-			//ignoring e
-			b = false;
-		}
-		return b;
-	}
-	
-	protected void setCell(BoolGrid grid, int row, int col, boolean alive) {
-		if(!grid.isValidCell(row, col))
-			throw new IndexOutOfBoundsException("Cell does not exists : ("+row+","+col+")");
-		grid.set(row, col, alive);
-	}
-	
-	public void setCurrentGenCell(int row, int col, boolean alive) throws IndexOutOfBoundsException{
-		setCell(this.currentGeneration,row,col,alive);
-	}
-	
-	public void setNextGenCell(int row, int col, boolean alive)  throws IndexOutOfBoundsException{
-		setCell(this.nextGeneration,row,col,alive);
-	}
-	
-	public void randomizeCells() {
-		this.clear();
-		for(int row=0; row<rows(); row++) {
-			for(int col=0; col<cols(); col++) {
-				setCurrentGenCell(row,col,Math.random() < 0.5);
-			}
-		}
-	}
-	
-	protected abstract void endAutomaton();
-	
-	public void end() {
-		this.end = true;
-	}
-	
-	@Override
-	public String toString() {
-		return this.end ? "Automaton ended at generation "+this.generation : this.currentGeneration.toString();
-	}
+    protected BoolGrid          currentGeneration;
+    protected BoolGrid          nextGeneration;
+    protected int               speed         = 1;
+    protected boolean           paused        = true;
+    protected int               generation    = 0;
+    protected boolean           end           = false;
+    protected AutomatonUpdater  updater;
+    public static final int     DEFAULT_SIZE  = 32;
+    protected List<EventTarget> evtTargetList = new ArrayList<EventTarget>();
+
+    public CellularAutomaton() {
+        this(DEFAULT_SIZE, DEFAULT_SIZE);
+    }
+
+    public CellularAutomaton(int size) {
+        this(size, size);
+    }
+
+    public CellularAutomaton(int rows, int cols) {
+        if (rows < 1 || cols < 1)
+            throw new IllegalArgumentException("Invalid dimensions : (" + rows + "," + cols + ")");
+        this.currentGeneration = new BoolGrid(rows, cols);
+        this.nextGeneration = new BoolGrid(rows, cols);
+        updater = new AutomatonUpdater(this);
+        init();
+    }
+
+    protected void fireAutomatonEvent(EventType<AutomatonEvent> type) {
+        for (EventTarget target : evtTargetList)
+            Event.fireEvent(target, new AutomatonEvent(this, target, type));
+    }
+
+    protected abstract void init();
+
+    public void clear() {
+        this.currentGeneration.reset();
+        this.nextGeneration.reset();
+        this.generation = 0;
+        this.end = false;
+        this.paused = true;
+    }
+
+    public void reset() {
+        clear();
+        init();
+        fireAutomatonEvent(AutomatonEvent.AUTOMATON_RESET);
+    }
+
+    public boolean isEnd() {
+        return this.end;
+    }
+
+    public void next() {
+        if (end)
+            return;
+        nextStep();
+        this.generation++;
+        this.currentGeneration.initWithOther(this.nextGeneration);
+        // this.nextGeneration.reset();
+        fireAutomatonEvent(AutomatonEvent.AUTOMATON_STEP);
+    }
+
+    protected abstract void nextStep();
+
+    public void pause() {
+        if (this.paused)// if already paused
+            return;
+        this.paused = true;
+        fireAutomatonEvent(AutomatonEvent.AUTOMATON_PAUSED);
+        try {
+            this.updater.cancel();
+        } catch (IllegalStateException e) {
+            // ignoring e
+        }
+    }
+
+    public void start() {
+        if (!this.paused)// if already started
+            return;
+        this.paused = false;
+        rescheduleUpdater();
+        fireAutomatonEvent(AutomatonEvent.AUTOMATON_STARTED);
+    }
+
+    public boolean isPaused() {
+        return this.paused;
+    }
+
+    public int getSpeed() {
+        return this.speed;
+    }
+
+    public void rescheduleUpdater() {
+        try {
+            updater.cancel();
+        } catch (IllegalStateException e) {
+            // ignoring e
+        }
+        updater = new AutomatonUpdater(this);
+    }
+
+    public void setSpeed(int value) {
+        if (value <= 0) {
+            this.speed = 1;
+        } else {
+            this.speed = value;
+            if (!this.paused)
+                rescheduleUpdater();
+        }
+    }
+
+    public int getGeneration() {
+        return this.generation;
+    }
+
+    public int rows() {
+        return this.currentGeneration.rows();
+    }
+
+    public int cols() {
+        return this.currentGeneration.cols();
+    }
+
+    public boolean isCellPopulated(int row, int col) {
+        boolean b;
+        try {
+            b = this.currentGeneration.get(row, col);
+        } catch (IndexOutOfBoundsException e) {
+            // ignoring e
+            b = false;
+        }
+        return b;
+    }
+
+    protected void setCell(BoolGrid grid, int row, int col, boolean alive) {
+        if (!grid.isValidCell(row, col))
+            throw new IndexOutOfBoundsException("Cell does not exists : (" + row + "," + col + ")");
+        grid.set(row, col, alive);
+    }
+
+    public void setCurrentGenCell(int row, int col, boolean alive) throws IndexOutOfBoundsException {
+        setCell(this.currentGeneration, row, col, alive);
+    }
+
+    public void setNextGenCell(int row, int col, boolean alive) throws IndexOutOfBoundsException {
+        setCell(this.nextGeneration, row, col, alive);
+    }
+
+    public void randomizeCells() {
+        this.clear();
+        for (int row = 0; row < rows(); row++) {
+            for (int col = 0; col < cols(); col++) {
+                setCurrentGenCell(row, col, Math.random() < 0.5);
+            }
+        }
+    }
+
+    protected abstract void endAutomaton();
+
+    public void end() {
+        this.end = true;
+        try {
+            updater.cancel();
+        } catch (IllegalStateException e) {
+            // ignoring e
+        }
+        endAutomaton();
+        fireAutomatonEvent(AutomatonEvent.AUTOMATON_ENDED);
+    }
+
+    public void addEventTarget(EventTarget target) {
+        evtTargetList.add(target);
+    }
+
+    public boolean removeEventTarget(EventTarget target) {
+        return evtTargetList.remove(target);
+    }
+
+    public void removeAllEventTargets() {
+        evtTargetList.clear();
+    }
+
+    public abstract Automatons getAutomatonType();
+
+    @Override
+    public String toString() {
+        String s = "============= GENERATION " + this.generation + " =============";
+        return this.end ? "Automaton ended at generation " + this.generation
+                : s + "\n" + this.currentGeneration.toString();
+    }
 }
